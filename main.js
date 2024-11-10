5d111ef3701962f0037dc81c9a1cbeff522f140d
ishaq iddriss <ishaqidris95@gmail.com>
2024-11-10 19:13:21 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+// const menuicon = document.querySelector('.menu-icon i'); // The menu icon
+// const links = document.querySelector('.links'); // The menu links
+
+// const closeicon = document.createElement('i');
+// closeicon.classList.add("material-symbols-outlined close" >
+//     close); // Add FontAwesome close icon classes
+// menuicon.appendChild(closeicon); 
+// // Function to remove the menu when clicked
+
+// menuicon.addEventListener('click', () => {
+//     if(links.style.display === 'none') {
+//         links.style.display = 'block'
+//     }
+//     else{
+//         links.style.display = 'none'
+//     }
+// })
