2fa148a0fdbfce3014bc53df7942825feded8a66
ishaq iddriss <ishaqidris95@gmail.com>
2025-01-02 16:43:46 +0000

    general change made

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
-// const menuicon = document.querySelector('.menu-icon i'); // The menu icon
-// const links = document.querySelector('.links'); // The menu links
+const menuicon = document.querySelector('.menu-icon'); // The menu icon
+const links = document.querySelector('.links'); // The menu links
 
 // const closeicon = document.createElement('i');
 // closeicon.classList.add("material-symbols-outlined close" >
@@ -9,11 +9,11 @@
 // menuicon.appendChild(closeicon); 
 // // Function to remove the menu when clicked
 
-// menuicon.addEventListener('click', () => {
-//     if(links.style.display === 'none') {
-//         links.style.display = 'block'
-//     }
-//     else{
-//         links.style.display = 'none'
-//     }
-// })
+menuicon.addEventListener('click', () => {
+    if(links.style.display === 'none') {
+        links.style.display = 'block'
+    }
+    else{
+        links.style.display = 'none'
+    }
+})
